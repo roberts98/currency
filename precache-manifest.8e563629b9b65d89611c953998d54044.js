169d65d6ea05797265588f5db588198ecfa05ca4
Robert Sidzinka <robert.sidzinka@abventor.com>
2020-06-17 20:52:15 +0200

    Updates

---
+++ precache-manifest.8e563629b9b65d89611c953998d54044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958938bd47b4aec319df0bb282091d11",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "ce4fc2c68eec981c018c",
+    "url": "/currency/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ce4fc2c68eec981c018c",
+    "url": "/currency/static/js/2.9006b547.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency/static/js/2.9006b547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff57600b147fd4d632d",
+    "url": "/currency/static/js/main.401babee.chunk.js"
+  },
+  {
+    "revision": "aa5bd6ab67c3382a3d60",
+    "url": "/currency/static/js/runtime-main.4d9ace83.js"
+  }
+]);
